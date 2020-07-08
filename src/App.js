e37a1a31b9c206c93d20772d4a2ad588b69b077a
veenashree97 <veenashree.k@namastecredit.com>
2020-07-08 16:15:19 +0530

    app component changed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.scss";
+import { Route, Switch, BrowserRouter } from "react-router-dom";
+import BarGraph from "../src/components/BarGraph";
+
+import Main from "./modules/main";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Main}></Route>
+        <Route path="/graph" exact component={BarGraph}></Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
