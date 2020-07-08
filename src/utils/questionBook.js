a845afa2f416daed6cde40a7ddcec1828b7a67b4
veenashree97 <veenashree.k@namastecredit.com>
2020-07-08 14:58:22 +0530

    utils file added

---
+++ src/utils/questionBook.js
@@ -0,0 +1,28 @@
+export const questionBook = [
+  {
+    question: "When was the last time India won the Cricket World Cup ?",
+    options: ["2003", "2004", "2010"],
+    ans: "2003",
+    selected: "",
+  },
+  {
+    question:
+      "What is the highest individual score by a batsman in Test Cricket ?",
+    options: ["400", "375", "650"],
+    ans: "375",
+    selected: "",
+  },
+  {
+    question: "Who has won the most number of Cricket World Cup ?",
+    options: ["Australia", "India", "England"],
+    ans: "Australia",
+    selected: "",
+  },
+  {
+    question:
+      "How many International centuries does Sachin Tendulkar has under his name ?",
+    options: ["52", "38", "49"],
+    ans: "49",
+    selected: "",
+  },
+];
